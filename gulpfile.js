01966a9262e271225e84d7d1ee284ff040f0535d
lekanda <abernaolao@gmail.com>
2021-10-13 22:24:32 +0200

    Gulp para compilar scss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-function tarea(done) {
-    console.log('desde la primera tarea');
-    // 'Done' es un callback de gulp qu le dice cuando acabar. Tmb vale 'fn' y 'callback'.
+// API de Gulp para trabajar con el.'src' para identificar el archivo. 'dest'
+const {src, dest} = require('gulp');
+// Con 'gulp-sass' conectamos con el compilador de SASS. Por eso hace falta 2 require
+const sass = require('gulp-sass')(require('sass'));
+// const sass = require('gulp-dart-sass'); // sustituye a las 2 anteriores.
+
+function css(done) {
+    // Identificar el archivo 'scss' a compilar
+    src('src/scss/app.scss') // 'pipe' hace esperar hasta que esta lista la anterior
+        .pipe(sass()) // Compilarlo
+        .pipe(dest('build/css')) // Almacenarla
+    
     done();
 }
-function tarea2(done) {
-    console.log('desde la segunda tarea');
-    done();
-}
-
-exports.tarea = tarea;
-exports.tarea2 = tarea2;
-
-// Ejecutar en terminal 'gulp tarea', 'gulp tarea2' para ejecutar funciones.
+exports.css = css;
