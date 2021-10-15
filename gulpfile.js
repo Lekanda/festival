67ff00ef35ae4900c9fa195a0049dce8e5246ac8
lekanda <abernaolao@gmail.com>
2021-10-15 19:38:11 +0200

    Automatizar con GULP la conversion a WEBP. Instalar gulp-webp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 // API de Gulp para trabajar con el.'src' para identificar el archivo. 'dest'
-const {src, dest,watch} = require('gulp');
+const {parallel,src, dest,watch} = require('gulp');
 // Con 'gulp-sass' conectamos con el compilador de SASS. Por eso hace falta 2 require
 const sass = require('gulp-sass')(require('sass'));
 // const sass = require('gulp-dart-sass'); // sustituye a las 2 anteriores.
 
 // Plumber
 const plumber = require('gulp-plumber');
+
+// Imagenes WEBP
+const webp = require('gulp-webp');
 
 
 // Compila SASS
@@ -18,9 +21,30 @@ function css(done) {
     done();
 }
 
+
+function versionWebp(done) {
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{jpg,png}') // Busca
+        .pipe(webp(opciones)) // convierte
+        .pipe(dest('build/img')) // guarda
+    done();
+}
+
+
+
+
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp,dev);
+// parallel: ejecuta todas a la vez
+// serie: una detras de otra
+
+
+
