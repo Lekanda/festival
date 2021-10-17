6e7230459fa97b11a8acf2d9804fb560849ec74c
lekanda <abernaolao@gmail.com>
2021-10-17 11:39:43 +0200

    Generar HTML de galeria con js.

--- src/js/app.js
+++ src/js/app.js
@@ -9,5 +9,21 @@ function iniciarApp() {
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
-    galeria.textContent = 'Vamos a crear la Galeria';
+    // galeria.textContent = 'Vamos a crear la Galeria';
+
+
+    for(let i=1; i<=12; i++){
+        // console.log(i);
+
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `;
+        console.log(imagen);
+        galeria.appendChild= imagen;
+    }
+    
+
 }
