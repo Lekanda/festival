15b0f5cecff4a3d6ed3690352223c161e9a47422
lekanda <abernaolao@gmail.com>
2021-10-17 13:43:26 +0200

    Galeria: Dar click a img y hacerla mas grande con fondo mas gris.

--- build/js/app.js
+++ build/js/app.js
@@ -22,6 +22,32 @@ function crearGaleria() {
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
         `;
         // console.log(imagen);
+
+        // MODO 1
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+        // MODO 2
+        // imagen.onclick = mostrarImagen;// Esta hace llamar la funcion automaticamente.
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(i) { 
+    // console.log('Mostrando ', i);
+
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${i}.avif" type="image/avif">
+        <source srcset="build/img/grande/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${i}.jpg" alt="imagen galeria">
+    `;
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
 }
